deff0a709453fcf9d2085558ca0879c83e48873a
Papaul-msibiii <gomispapaul6@gmail.com>
2025-04-02 11:46:56 -0400

    configure favorite slice

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -8,7 +8,17 @@ const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-   
+   toggleFavorite: (state, action) => {
+        const recipe = action.payload; 
+        const existingIndex = state.favoriterecipes.findIndex(
+            (item) => item.recipeId === recipe.recipeId
+        );
+        if (existingIndex >= 0) {
+            state.favoriterecipes.splice(existingIndex, 1); // Retirer des favoris
+        } else {
+            state.favoriterecipes.push(recipe); // Ajouter aux favoris
+        }
+    }
   },
 });
 
